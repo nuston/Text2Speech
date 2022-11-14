eacddc4819b32c346e204571988223ae1d9adf8d
nuston_pay <43966559+nuston@users.noreply.github.com>
2022-11-14 12:43:50 +0100

    Add files via upload

---
+++ Text2Speech.js
@@ -0,0 +1,31 @@
+const btn = document.querySelector('#btn');
+const text = document.querySelector('#text');
+const speech = window.speechSynthesis;
+let utter;
+
+text.oninput = () => {
+  utter = new SpeechSynthesisUtterance(text.value);
+  utter.pitch = 0.8;
+  utter.rate = 0.5;
+}
+
+const speakOut = () => {
+  if (!speech.speaking) {
+    speech.speak(utter);
+    btn.innerHTML = `<span>stop</span>`;
+  } else if (speech.speaking){
+    speech.cancel();
+  }
+  utter.onend = () => btn.innerHTML = `<span>Tap-2-Speak</span>`
+}
+
+btn.onclick = e => {
+  e.preventDefault();
+  speakOut();
+}
+
+//date
+let cYear = new Date().getFullYear();
+
+let copy = document.querySelector('#copy');
+copy.innerHTML = `&copy; ${cYear}`;
